e9ab5f674856cb949287b98019df7686853f39b9
user <user@DESKTOP-6JBEF5K>
2022-03-28 11:30:57 +0800

    OK

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,9 +12,33 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        private void listen() { }
+
+        private string Myip() { }
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
         }
     }
 }
